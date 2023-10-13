03d008ad6a3d0423bbc839fa57d42bd051220ed0
manishupadhyay911 <upadhyaymanish302@gmail.com>
2023-10-13 19:34:30 +0530

    updated url

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -23,8 +23,8 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    // const s = io("https://colab-ywka.onrender.com")
-    const s = io("http://localhost:3001")
+    const s = io("https://colab-ywka.onrender.com")
+    // const s = io("http://localhost:3001")
     setSocket(s)
 
     return () => {
